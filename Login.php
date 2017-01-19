2b0fd1a0fbb0a2732bad78afa22b57f6a7c9fe37
Nkirui <nathankirui5@gmail.com>
2017-01-19 23:41:37 +0300

    added php files

---
+++ Login.php
@@ -0,0 +1,31 @@
+<?php
+   ini_set('display_errors', 'On');
+   $con = mysqli_connect("localhost", "root", "monkey567", "REGISTRATION");
+    
+if(isset($_POST["username"]) && isset($_POST["password"]))
+{
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+}
+    $statement = mysqli_prepare($con, "SELECT * FROM user WHERE username = ? AND password = ?");
+    mysqli_stmt_bind_param($statement, "ss", $username, $password);
+    mysqli_stmt_execute($statement);
+    
+    mysqli_stmt_store_result($statement);
+    mysqli_stmt_bind_result($statement, $user_id, $name, $username, $age, $password);
+    
+    $response = array();
+    $response["success"] = false;  
+    
+    while(mysqli_stmt_fetch($statement)){
+        $response["success"] = true;  
+        $response[""] = $name;
+        $response[""] = $username;
+        $response[""] = $age;
+        $response[""] = $password;
+    }
+    
+    //echo json_encode($response);
+    print_r(json_encode($response));
+?>
+
---
+++ Register.php
@@ -0,0 +1,22 @@
+<?php
+
+  ini_set('display_errors', 'On');
+    $con = mysqli_connect("localhost", "root", "monkey567", "REGISTRATION");
+
+    if(isset($_POST["name"]) && isset($_POST["username"]) && isset($_POST["age"]) && isset($_POST["password"]))
+    {
+    $name = $_POST["name"];
+    $username = $_POST["username"];
+    $age = $_POST["age"];
+    $password = $_POST["password"];
+    }
+    $statement = mysqli_prepare($con, "INSERT INTO user(name, username, age, password) VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "ssis", $name, $username, $age, $password);
+    mysqli_stmt_execute($statement);
+    
+    $response = array();
+    $response["success"] = true;  
+    
+   echo json_encode($response);
+  // print_r(json_encode($response));
+?>
